5fc3cf26ac319d4f1a611c684d75f42c1b917be2
marcus nilsson <marcusnilsson2010@gmail.com>
2023-05-17 16:42:09 +0200

    learning useState

---
+++ src/components/Counter.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+const Counter = () => {
+
+    const [value, setValue] = useState(0);
+
+  
+  
+    const plus = () =>{
+      // setValue(value + 1)
+      setTimeout(()=>{
+        console.log('button is CLICKED');
+  
+          setValue((currentState )=>{
+          return currentState + 1
+        });
+      }, 3000)
+        
+    };
+  
+    const reset = () =>{
+      setValue(0)
+    };
+  
+    return (
+      <div className="container">
+        <p> useState Counter  with  "setTimeout"  3sek</p>
+        <h1> count = <span>{value}</span> </h1>
+        <button className="increas btn" onClick={plus}>Increas +</button>
+        <button className="reset btn" onClick={reset}>RESET</button>
+      </div>
+    )
+    
+}
+export default Counter;
+
+
